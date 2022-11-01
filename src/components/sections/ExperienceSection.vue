59c4b20c6f5778a833f7041119aa618a8a2aafe3
Kodeneko <ladysun.freedom@gmail.com>
2022-11-01 18:34:29 +0100

    [master] i18n + experience

--- src/components/sections/ExperienceSection.vue
+++ src/components/sections/ExperienceSection.vue
@@ -1,13 +1,5 @@
 <template>
-  <p>
-    Llevo casi 7 años dedicandome al desarrollo. Desde que era joven me gustaba
-    hacer webs en la época del HTML4. Cuando crecí quise ser artista y vi que no
-    era mi camino. Decidí estudiar ingeniería informática y al terminar comence
-    haciendo aplicaciones móbiles con Android. Al poco comence con el desarrollo
-    web y me especialicé en el desarrollo front. Trabajé principalmente en
-    consultorias en proyectos para medianas y grandes compañías como Odigeo,
-    Inditex o la WIPO/EUIPO.
-  </p>
+  <p>{{ $t("exp.text") }}</p>
 </template>
 
 <script>
